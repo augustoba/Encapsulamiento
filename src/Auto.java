681f1f92d0f3cf07dad78c0c5f178eda22191c76
augusto <94504327+augustoba@users.noreply.github.com>
2023-04-10 20:31:54 -0300

    first commit

---
+++ src/Auto.java
@@ -0,0 +1,44 @@
+public class Auto {
+
+    private String color;
+    public Integer  puertas;
+    Integer modelo;
+    protected int cc;
+
+
+    public Auto() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getPuertas() {
+        return puertas;
+    }
+
+    public void setPuertas(Integer puertas) {
+        this.puertas = puertas;
+    }
+
+    public Integer getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Integer modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCc() {
+        return cc;
+    }
+
+    public void setCc(int cc) {
+        this.cc = cc;
+    }
+
+}
---
+++ src/Encapsulamiento.java
@@ -0,0 +1,10 @@
+public class Encapsulamiento {
+    public static void main(String[] args) {
+
+            Auto objAuto = new Auto();
+        objAuto.setColor("red");
+
+        System.out.println(objAuto.getColor());
+
+    }
+}
